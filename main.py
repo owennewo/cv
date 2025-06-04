f82011aee65141655ff7215aaa6db4d0e8eb6751
Owen Williams <williams.owen@gmail.com>
2025-06-04 22:24:38 +0100

    adding template

---
+++ main.py
@@ -0,0 +1,31 @@
+from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
+import json
+from datetime import datetime, UTC
+
+def main():
+    with Path("cv.json").open("r", encoding="utf-8") as f:
+        data = json.load(f)
+        data["current_year"] = datetime.now(tz=UTC).year
+
+        print(data)
+        if "social_links" in data:
+            for link in data["social_links"]:
+                if link.get("svg_path"):
+                    with Path(link["svg_path"]).open(encoding="utf-8") as svg_file:
+                        link["svg_data"] = svg_file.read()
+
+        # Set up Jinja environment
+        env = Environment(loader=FileSystemLoader("."), autoescape=True)
+        cv_template = env.get_template("cv_template.html")
+
+        cv_output = cv_template.render(**data)
+
+        # Write the output to an HTML file
+        with Path("cv.html").open("w", encoding="utf-8") as f:
+            f.write(cv_output)
+
+        print("HTML file generated successfully!")
+
+if __name__ == "__main__":
+    main()
